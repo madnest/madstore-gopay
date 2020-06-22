99d84e014afcffcbc784fd0e76e103307b0f9989
Jakub Theimer <theimer@madne.st>
2020-06-22 03:11:57 +0200

    remove contracts

--- src/Contracts/PaymentGateway.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Madnest\MadstoreGopay\Contracts;
-
-interface PaymentGateway
-{
-    public function createPayment(Purchasable $purchasable, array $params = [], array $options = []);
-}

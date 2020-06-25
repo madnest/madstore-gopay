13299a342410c2c5e40a1fa813830447008af9ae
Jakub Theimer <theimer@madne.st>
2020-06-26 01:51:43 +0200

    add amount and currency

--- src/MadstoreGopay.php
+++ src/MadstoreGopay.php
@@ -87,6 +87,8 @@ class MadstoreGopay implements PaymentOption
         return $this
             ->newPaymentResponse($response->statusCode, $response->json['state'])
             ->setOrderNumber($response->json['order_number'])
+            ->setAmount($response->json['amount'])
+            ->setCurrency($response->json['currency'])
             ->setPaymentMethod($response->json['payment_instrument'])
             ->setRedirectUrl($response->json['gw_url']);
     }
